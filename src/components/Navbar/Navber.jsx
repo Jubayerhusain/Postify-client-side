9a6c8e6c0668fa8a92547ff8c483bb5397341b22
Jubayer husain <jubayerhusai360@gmail.com>
2024-11-29 10:48:19 +0600

    Navber update done with color

--- src/components/Navbar/Navber.jsx
+++ src/components/Navbar/Navber.jsx
@@ -1,68 +1,10 @@
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Link, useNavigate, NavLink } from "react-router-dom";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import { useContext } from "react";
 
 function Navber() {
   const { signOutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
-  const location = useLocation();
-
-  const isActive = (path) => location.pathname === path;
-
-  const link = (
-    <>
-      <li>
-        {user && (
-          <Link
-            to="/profile"
-            className={`${
-              isActive("/profile") ? "text-blue-400 font-bold" : "text-gray-400"
-            } hover:text-blue-300`}
-          >
-            Profile
-          </Link>
-        )}
-      </li>
-      <li>
-        {user && (
-          <Link
-            to="/userHome"
-            className={`${
-              isActive("/home") ? "text-blue-400 font-bold" : "text-gray-400"
-            } hover:text-blue-300`}
-          >
-            Home
-          </Link>
-        )}
-      </li>
-      <li>
-        {user && (
-          <Link
-            to="/video"
-            className={`${
-              isActive("/video") ? "text-blue-400 font-bold" : "text-gray-400"
-            } hover:text-blue-300`}
-          >
-            Video
-          </Link>
-        )}
-      </li>
-      <li>
-        {user && (
-          <Link
-            to="/notification"
-            className={`${
-              isActive("/notification")
-                ? "text-blue-400 font-bold"
-                : "text-gray-400"
-            } hover:text-blue-300`}
-          >
-            Notification
-          </Link>
-        )}
-      </li>
-    </>
-  );
 
   const handleSignOut = () => {
     signOutUser()
@@ -72,14 +14,72 @@ function Navber() {
       .catch((error) => console.log(error));
   };
 
+  const navLinks = (
+    <>
+      {user && (
+        <>
+          <li>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) =>
+                `hover:text-blue-400 ${
+                  isActive ? "text-blue-400 font-bold" : "text-gray-400"
+                }`
+              }
+            >
+              Profile
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/userHome"
+              className={({ isActive }) =>
+                `hover:text-blue-400 ${
+                  isActive ? "text-blue-400 font-bold" : "text-gray-400"
+                }`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/video"
+              className={({ isActive }) =>
+                `hover:text-blue-400 ${
+                  isActive ? "text-blue-400 font-bold" : "text-gray-400"
+                }`
+              }
+            >
+              Video
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/notification"
+              className={({ isActive }) =>
+                `hover:text-blue-400 ${
+                  isActive ? "text-blue-400 font-bold" : "text-gray-400"
+                }`
+              }
+            >
+              Notification
+            </NavLink>
+          </li>
+        </>
+      )}
+    </>
+  );
+
   return (
     <div className="py-4 navbar px-10 bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700 text-white shadow-lg">
+      {/* Left Side */}
       <div className="navbar-start">
         <div className="dropdown">
-          <div
+          <button
             tabIndex={0}
-            role="button"
             className="btn btn-ghost lg:hidden text-white"
+            aria-label="menu"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -95,32 +95,39 @@ function Navber() {
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
-          </div>
+          </button>
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-gray-800 text-gray-400 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
-            {link}
+            {navLinks}
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-2xl text-blue-500 font-bold">
+        <Link
+          to="/"
+          className="btn btn-ghost normal-case text-2xl text-blue-500 font-bold"
+        >
           Postify
-        </a>
+        </Link>
       </div>
+
+      {/* Center: Navbar Links */}
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 text-lg">{link}</ul>
+        <ul className="menu menu-horizontal px-1 text-lg">{navLinks}</ul>
       </div>
+
+      {/* Right Side */}
       <div className="navbar-end">
         {user ? (
           <button
             onClick={handleSignOut}
-            className="btn bg-gray-700 hover:bg-gray-600 text-blue-400 font-semibold border-blue-400"
+            className="btn bg-gray-700 hover:bg-gray-600 text-blue-400 font-semibold border border-gray-500"
           >
             Sign Out
           </button>
         ) : (
           <Link
-            to={"/"}
+            to="/signUp"
             className="btn bg-blue-600 hover:bg-blue-500 text-white font-semibold border-blue-400"
           >
             Sign Up
